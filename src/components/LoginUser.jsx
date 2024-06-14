3f4e4a1dfff907ea2bfd5d99de9ce16839f84aa9
Annaeldho01 <annaeldho001@gmail.com>
2024-06-14 10:45:24 +0530

    project completed

--- src/components/LoginUser.jsx
+++ src/components/LoginUser.jsx
@@ -41,7 +41,7 @@ const LoginUser = () => {
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <div className="row g-3">
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <label htmlFor="" className="form-label"><b>EMAILID</b></label>
+                            <label htmlFor="" className="form-label"><b>Email</b></label>
                             <input type="text" className="form-control"  name='email' value={data.email} onChange={inputhandler}/>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
